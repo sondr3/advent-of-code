5626751b2d5eaeb728795ef1d398960afde69ae7
Sondre Aasemoen <sondre@eons.io>
2024-12-03 11:31:52 +0100

    Solve 2024-03

---
+++ src/Year/Y24/Day03.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
+module Year.Y24.Day03 where
+
+import Data.Functor (($>))
+import Data.Maybe (catMaybes)
+import Day (AoC, mkAoC)
+import Parsers (Parser, lexeme, parens, symbol)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as L
+
+type Input = [Instruction]
+
+data Instruction
+  = Mul Int Int
+  | Enable
+  | Disable
+  deriving stock (Show, Eq)
+
+insValue :: Instruction -> Int
+insValue (Mul x y) = x * y
+insValue _ = 0
+
+partA :: Input -> Int
+partA = foldl' (\acc i -> acc + insValue i) 0
+
+partB :: Input -> Int
+partB xs = foldl' (\acc i -> acc + insValue i) 0 (go xs [] True)
+  where
+    go (m@(Mul _ _) : xss) acc s = if s then go xss (m : acc) s else go xss acc s
+    go (Enable : xss) acc _ = go xss acc True
+    go (Disable : xss) acc _ = go xss acc False
+    go [] acc _ = acc
+
+parser :: Parser Input
+parser = catMaybes <$> some (go <* optional eol) <* eof
+  where
+    go =
+      choice
+        [ Just <$> try parseMul,
+          Just Enable <$ try (symbol "do()"),
+          Just Disable <$ try (symbol "don't()"),
+          anySingle $> Nothing
+        ]
+
+parseMul :: Parser Instruction
+parseMul = symbol "mul" *> parens (Mul <$> (lexeme L.decimal <* symbol ",") <*> lexeme L.decimal)
+
+day03 :: AoC
+day03 = mkAoC parser partA partB 3 2024
--- test/Y24Spec.hs
+++ test/Y24Spec.hs
@@ -6,8 +6,10 @@ import Test.Hspec (Spec, parallel)
 import TestUtils (testDay)
 import Year.Y24.Day01 (day01)
 import Year.Y24.Day02 (day02)
+import Year.Y24.Day03 (day03)
 
 spec :: Spec
 spec = parallel $ do
   testDay day01
   testDay day02
+  testDay day03
